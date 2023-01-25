61d6c5b6b33a83a966e3d23c8f1733fcd91c51df
Brundex <puodziunasb@gmail,com>
2023-01-25 01:43:39 -0300

    add book to library onsubmit

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+document.getElementById("form").onsubmit = function(event) {
+    event.preventDefault();
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const newBook = new Book(title, author, pages);
+    myLibrary.push(newBook);
+    console.table(myLibrary);
+}
 
+let myLibrary = [];
+
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+
+// function createNewBook() {
+//     const title = document.getElementById("title").value;
+//     const author = document.getElementById("author").value;
+//     const pages = document.getElementById("pages").value;
+//     const newBook = new Book(title, author, pages);
+//     myLibrary.push(newBook);
+// }
+
+console.log(myLibrary);
+
+
+// function addBookToLibrary(_book) {
+
+// }
+
+   
+
+  
